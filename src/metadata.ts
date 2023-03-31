b7bfbfe22926d5859a58e130486f1726af13bf1a
Andrey Makarov <viva.la.akam@gmail.com>
2023-03-31 09:58:44 +0400

    fix: metadata base64 coding

--- src/metadata.ts
+++ src/metadata.ts
@@ -3,6 +3,7 @@ import {accountInfo, sign} from '@haqq/provider-web3-utils';
 import {jsonrpcRequest} from './jsonrpc-request';
 import {decrypt, encrypt} from '@haqq/encryption-react-native';
 import stringify from 'json-stable-stringify';
+import base64 from 'react-native-base64';
 
 async function signMetadata(
   privateKey: BN | string,
@@ -20,7 +21,7 @@ async function signMetadata(
     }),
   );
 
-  const {publicKeyUncompressed} = await accountInfo(privateKey.toString('hex'));
+  const {publicKeyUncompressed} = await accountInfo(pk);
 
   return [publicKeyUncompressed, key, signature, timestamp, value];
 }
@@ -30,6 +31,7 @@ export async function getMetadataValue(
   privateKey: BN | string,
   key: string,
 ) {
+  const pk = BN.isBN(privateKey) ? privateKey.toString('hex') : privateKey;
   const params = await signMetadata(privateKey, key);
 
   const result = await jsonrpcRequest<{
@@ -37,9 +39,11 @@ export async function getMetadataValue(
     value: string;
   }>(host, 'get', params);
 
+  const value = base64.decode(result.value || '');
+
   const message = await decrypt<{
     value: any;
-  }>(privateKey.toString('hex'), result.value);
+  }>(pk, value);
   return message.value;
 }
 
@@ -50,7 +54,8 @@ export async function setMetadataValue(
   value: any,
 ) {
   const message = await encrypt(privateKey.toString('hex'), {value});
-  const params = await signMetadata(privateKey, key, message);
+  const b64message = base64.encode(message);
+  const params = await signMetadata(privateKey, key, b64message);
 
   const result = await jsonrpcRequest<{
     key: string;
