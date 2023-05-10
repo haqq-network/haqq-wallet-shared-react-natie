80c08227112bb6c6912424c2d6a49239856d5480
Andrey Makarov <viva.la.akam@gmail.com>
2023-05-10 08:19:16 +0400

    feat: shares

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export * from './jsonrpc-request';
 export * from './metadata';
 export * from './types';
 export * from './make-id';
+export * from './share'
--- src/types.ts
+++ src/types.ts
@@ -6,3 +6,19 @@ export type SharesResponse = {
 export type ShareCreateResponse = {
   hex_share: string;
 };
+
+export type Share = {
+  share: string;
+  shareIndex: string;
+  polynomialID: string;
+  name?: string;
+};
+
+export type ShareEncrypted = {
+  nonce: string;
+  shareIndex: string;
+  polynomialID: string;
+  publicShare: string;
+  name?: string;
+};
+
